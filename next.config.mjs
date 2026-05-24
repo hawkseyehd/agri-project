9e7c31c1da62d7b10dfc7231791d183c3404a2ed
Safeer Bhatti <safeerbhatti007@yahoo.com>
2026-05-24 16:38:58 +0500

    fixes attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      "*": [
+        ".pnpm-store/**",
+        "node_modules/.pnpm/@playwright+test*/**",
+        "node_modules/.pnpm/playwright*/**",
+        "node_modules/.pnpm/next*playwright*/**",
+        "Plan/**",
+        "docs/**",
+        ".git/**",
+      ],
+    },
+  },
+};
 
 export default nextConfig;
